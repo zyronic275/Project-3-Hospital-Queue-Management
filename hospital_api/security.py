535b313b9798dc1ade49342d9d37efb32fc1bd54
justinrpangestu <154042559+justinrpangestu@users.noreply.github.com>
2026-02-17 23:02:32 +0700

    commit dikit

--- hospital_api/security.py
+++ hospital_api/security.py
@@ -1,4 +1,7 @@
 # FILE: security.py
+import os
+from dotenv import load_dotenv
+from jose import jwt
 from datetime import datetime, timedelta
 from typing import Optional
 from jose import JWTError, jwt
@@ -6,11 +9,11 @@ from passlib.context import CryptContext
 from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
 
-# KUNCI RAHASIA (Ganti dengan string acak panjang untuk production)
-SECRET_KEY = "kunci_rahasia_rumah_sakit_ini_sangat_aman_sekali"
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 60
+load_dotenv()
 
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM", "HS256")
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 60))
 pwd_context = CryptContext(schemes=["argon2"], deprecated="auto")
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login")
 
